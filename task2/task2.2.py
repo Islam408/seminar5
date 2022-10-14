610b01a0ac1f3010bd8ccf24b760426a8f7741fc
islam <mamaev408@gmail.com>
2022-10-14 19:52:26 +0500

    task2.2

---
+++ task2/task2.2.py
@@ -0,0 +1,66 @@
+from random import *
+import os
+
+#приветсвие
+weltext = ('ПРИВЕТСТВУЮ ИГРОКИ!, Добро пожаловать в игру КОНФЕТКИ!\n'
+                'ДИСКЛЭЙМЕР! \n'
+                'Не рекомендуется лицам с сахарным диабетом')
+print(weltext)
+
+message = ['твоя очередь', 'твой шанс', 'бери больше', 'ну же\n',
+           'бери быстрее', 'ты справишься','прощяй диета']
+winmessage =['мои поздваления', 'ты настоящий мастер игры','не проигравший сегодня\n'
+              ,'да здравтвует диабет\n'
+              ,'мог проиграть но выиграл','ты честно выиграл эту игру',]
+import random
+
+random_index = random.randrange(len(winmessage))
+
+
+def player_vs_bot():
+    candies_total = 100
+    max_take = 28
+    
+    player1 = input('Имя игрока: ')
+    player2 = 'Bot'
+    players = [player1,player2]
+
+    
+    x = randint(1,2)
+    
+    if x == 1:
+        lucky = player1
+        loser = player2
+    else:
+        lucky = player2
+        loser = player1
+    print(f'Поздравляю {lucky} ты ходишь первым !')
+    lucky = randint(1, 2)
+# ход игры
+    while candies_total > 0:
+        lucky += 1
+        if players[lucky % 2]== 'Bot':
+            print(f'\n Ходит {players[lucky % 2]} \n Осталось {candies_total}.конфет. {choice(message)}: ')
+
+
+            if candies_total > 0:
+                step = candies_total
+            
+            while step > max_take:
+                step = randint(1,28)
+            print(step)
+
+        else:
+            step = int(input(f'\nХодит,  {players[lucky%2]} \n Осталось {candies_total} конфет. {choice(message)}: '))
+            while step > max_take or step > candies_total:
+                step = int (input(f'\nМожно взять только {max_take} конфет {loser}, играй по правилам: '))
+        candies_total = candies_total - step
+
+
+    print(f'Осталось {candies_total} конфет.')
+    print(f'{winmessage[random_index]} Победитель {players[lucky%2]} ')
+
+
+
+player_vs_bot()
+                
--- task2/task2.py
+++ task2/task2.py
@@ -31,7 +31,6 @@ def player_vs_player():
     player_1 = input('Имя первого игрока: ')
     player_2 = input('Имя второго игрока: ')
 
-
     #определяем кто ходит первым
 
     x = randint(1, 2)
@@ -82,7 +81,52 @@ player_vs_player()
 
 
 
+def player_vs_bot():
+    candies_total = 10
+    max_take = 28
+    count = 0
+    player_1 = input('Имя первого игрока: ')
+    player_2 = 'Bot'
+    players =[player_1,player_2]
 
+    #определяем кто ходит первым
 
+    x = randint(1, 2)
+    if x == 1:
+        lucky = player_1
+        loser = player_2
+    else:
+        lucky = player_2
+        loser = player_1
+    #print(f'Поздравляю {lucky} ты ходишь первым !')
+    while candies_total > 0:
+        lucky += 1
+
+        if players[lucky % 2] == 'Компьютер':
+            print(
+                f'\nХодит {players[lucky%2]} \nНа кону {candies_total}. \n{choice(message)}: ')
+
+            if candies_total < 29:
+                step = candies_total
+            else:
+                delenie = candies_total//28
+                step = candies_total - ((delenie*max_take)+1)
+                if step == -1:
+                    step = max_take -1
+                if step == 0:
+                    step = max_take
+            while step > 28 or step < 1:
+                step = randint(1,28)
+            print(step)
+        else:
+            step = int(input(f'\nХод,  {players[lucky%2]} \n {candies_total} {choice(message)}:  '))
+            while step > max_take or step > candies_total:
+                step = int(input(
+                    f'\nМожно взять только {max_take} конфет {loser}, играй по правилам: '))
+        candies_total = candies_total - step
+
+    print(f'На кону осталось {candies_total} \nПобедил {players[lucky%2]}')
+
+player_vs_bot()
 
 
